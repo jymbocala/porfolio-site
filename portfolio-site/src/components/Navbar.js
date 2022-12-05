4ddbd5784172ac4a32a5fe0ba3b7ce6264abf9c8
Jym Bocala <jym.bocala@gmail.com>
2022-12-05 13:06:00 +1100

    add navbar and hamburger elements

--- portfolio-site/src/components/Navbar.js
+++ portfolio-site/src/components/Navbar.js
@@ -1,9 +1,30 @@
 import React from "react";
 
 export default function Navbar() {
+  
+
   return (
-    <div>
-      <p>Navbar</p>
+    <div className="navbar">
+      <p className="logo">LOGO</p>
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <a href="#" className="nav-link">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">About Me</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">Contact Me</a>
+        </li>
+      </ul>
+      <div className="hamburger">
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
     </div>
   )
 }
