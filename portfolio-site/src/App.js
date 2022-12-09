7de1eef55d5d08e2632c5955e9beec7550f267b7
Jym Bocala <jym.bocala@gmail.com>
2022-12-09 23:02:13 +1100

    add basic project card elements

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -4,6 +4,7 @@ import "./styles/queries.css";
 import Header from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
+import ProjectCard from "./components/ProjectCard";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,6 +14,13 @@ function App() {
       <main>
         <Hero />
         <AboutMe />
+        <section className="projects" id="projects">
+          <div className="projects-inner-container">
+            <h2 className="projects-title">Projects</h2>
+            <ProjectCard />
+          </div>
+
+        </section>
       </main>
       <Footer />
     </div>
---
+++ portfolio-site/src/components/ProjectCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ProjectCard() {
+  return (
+    <div className="project-card">
+      <img />
+      <h3>Project Title</h3>
+      <p>Project Description</p>
+      <ul>
+        <li>Highlight one</li>
+        <li>Highlight two</li>
+      </ul>
+      <p>Tech used:</p>
+    </div>
+  )
+}
